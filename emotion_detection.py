8356c0f59d7ffc3653341eb7f1cd100e175a09fc
olegrtecno <olegrtecno@gmail.com>
2025-01-03 05:20:30 -0500

    Formatted output

--- emotion_detection.py
+++ emotion_detection.py
@@ -16,8 +16,22 @@ def emotion_detector(text_to_analyse):
     # The actual API call
     response = requests.post(url, json = input_json, headers = header)
 
-    # Return the response body in case of success
+    # Return the emotions dictionary in case of success
     if (response.status_code == 200):
-        return response.text
+        
+        # Format the response as json
+        formatted_response = json.loads(response.text)
+
+        # Extract the emotions dictionary based on the response json structure: dictionary -> list -> dictionary
+        emotions = formatted_response["emotionPredictions"][0]["emotion"]
+
+        # Find the dominant emotion based on the maximum score
+        dominant_emotion = max(emotions, key = emotions.get)
+
+        # Add a new dictionary entry to indicate the dominant emotion
+        emotions["dominant_emotion"] = dominant_emotion
+
+        return emotions
     else:
-        return "Error occurred. Try again"
+        # Return the empty dictionary in case of error
+        return {}
